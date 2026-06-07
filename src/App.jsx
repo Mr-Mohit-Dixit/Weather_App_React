f3a79c4b848efc12709d609305e7f54a1a53cbd0
Mr-Mohit-Dixit <career.mohitdixit@gmail.com>
2026-06-07 07:30:11 +0530

    Create weather metrics section

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ const App = () => {
     <div className='container'>
 
       <h1> Weather App </h1>
-      <section>
-        {/* Search Section */}
+      <section className="search-section">
         <input
         type='text'
         placeholder='Enter City Name' 
@@ -21,18 +20,41 @@ const App = () => {
         <div className='weather-card'>
           <h2>City, Country</h2>
 
-          <p>Day, DD MM YYYY | Time</p>
+          <p>Sunday, 07 Jun 2026 | 07:30 AM</p>
 
           <h1>28°C</h1>
 
           <p>Cloudy</p>
 
-          <div>
+          <div className="temp-range">
             <span>Max: 31°C</span>
             <span>Min: 24°C</span>
           </div>
 
         </div>
+      </section>
+
+      <section className="metrics-section">
+        <div className='metric-card'>
+          <h4>Humidity</h4>
+          <p>70%</p>  
+        </div>
+
+        <div className='metric-card'>
+          <h4>Wind Speed</h4>
+          <p>5 km/h</p>  
+        </div>
+
+        <div className='metric-card'>
+          <h4>Pressure</h4>
+          <p>1212hPa</p>  
+        </div>
+
+        <div className='metric-card'>
+          <h4>Visibility</h4>
+          <p>10 Km</p>  
+        </div> 
+
       </section>
 
     </div>
