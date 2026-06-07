cb7af82cad0d3522b7ea454808109bb3e6a9bf0d
Mr-Mohit-Dixit <career.mohitdixit@gmail.com>
2026-06-07 08:53:43 +0530

    Convert weather UI to state drivern rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  let [city,setCity] = useState("");  
+  let [weather,setWeather] = useState({ /* setting mumbai as default*/
+                                        city: "Mumbai",
+                                        country: "In",
+                                        temperature: 28,
+                                        condition: "cloudy",
+                                        maxTemp: 31,
+                                        minTemp: 24,
+                                        humidity: 70,
+                                        windSpeed: 5,
+                                        pressure: 1212,
+                                        visibility: 10
+                                      });
+
+  const handleSearch = () => {
+
+    if(!city.trim())
+    {
+      alert("Enter a city name");
+      return;
+    }
+
+    setWeather({
+    city: city,
+    country: "IN",
+    temperature: 35,
+    condition: "Sunny",
+    maxTemp: 38,
+    minTemp: 29,
+    humidity: 50,
+    windSpeed: 10,
+    pressure: 1008,
+    visibility: 8
+    });
+
+    console.log(city);
+  }
+
   return (
     <div className='container'>
 
@@ -9,26 +48,28 @@ const App = () => {
         <input
         type='text'
         placeholder='Enter City Name' 
+        value={city}
+        onChange={(e)=>setCity(e.target.value)}
         />
 
-        <button>
+        <button onClick={handleSearch}>
           Search
         </button>
       </section>
 
       <section  className='weather-section'>
         <div className='weather-card'>
-          <h2>City, Country</h2>
+          <h2>{weather.city}, {weather.country}</h2>
 
           <p>Sunday, 07 Jun 2026 | 07:30 AM</p>
 
-          <h1>28°C</h1>
+          <h1>{weather.temperature}°C</h1>
 
-          <p>Cloudy</p>
+          <p>{weather.condition}</p>
 
           <div className="temp-range">
-            <span>Max: 31°C</span>
-            <span>Min: 24°C</span>
+            <span>Max: {weather.maxTemp}°C</span> 
+            <span>Min: {weather.minTemp}°C</span>
           </div>
 
         </div>
@@ -37,22 +78,22 @@ const App = () => {
       <section className="metrics-section">
         <div className='metric-card'>
           <h4>Humidity</h4>
-          <p>70%</p>  
+          <p>{weather.humidity}%</p>  
         </div>
 
         <div className='metric-card'>
           <h4>Wind Speed</h4>
-          <p>5 km/h</p>  
+          <p>{weather.windSpeed} km/h</p>  
         </div>
 
         <div className='metric-card'>
           <h4>Pressure</h4>
-          <p>1212hPa</p>  
+          <p>{weather.pressure} hPa</p>  
         </div>
 
         <div className='metric-card'>
           <h4>Visibility</h4>
-          <p>10 Km</p>  
+          <p>{weather.visibility} Km</p>  
         </div> 
 
       </section>
